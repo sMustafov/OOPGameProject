1a5bae354bfedf6aa9d537b301eb331dd86126a0
Sevgin Mustafov <sevgin95@gmail.com>
2015-11-27 14:57:36 +0200

    Starting the Project

---
+++ SoftUniBattleShip/SoftUniBattleShip/Players/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace SoftUniBattleShip.Players
+{
+    public class Player
+    {
+        private string userName;
+        private int points;
+        private int damage;
+
+        public Player(string userName, int points, int damage)
+        {
+            this.UserName = userName;
+            this.Points = points;
+            this.Damage = damage;
+        }
+
+        public string UserName { get; set; }
+
+        public int Points { get; set; }
+
+        public int Damage { get; set; }
+    }
+}
---
+++ SoftUniBattleShip/SoftUniBattleShip/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniBattleShip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
